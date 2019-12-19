732eadf4d7b9b7f5b44ada61364952edd1b1c755
Ali Esenli <aliriza.esenli@hotmail.com>
2019-12-19 15:43:00 +0100

    fixed sidemenu appearance on window resize

--- DirectSync/wwwroot/js/sidemenutoggle.js
+++ DirectSync/wwwroot/js/sidemenutoggle.js
@@ -1,6 +1,18 @@
 ﻿window.addEventListener("load", function () {
-    var toggler = document.getElementById("sidebar-toggler").addEventListener("click", function () {
-        document.body.classList.toggle("drawer-sidebar");
+    var content = document.getElementById("content-area");
+
+    document.getElementById("sidebar-toggler").addEventListener("click", function () {
+        var windowSize = window.innerWidth;
+        if (windowSize > 650 && !document.body.classList.contains("drawer-sidebar")) {
+            content.style = "padding-left: 0px;";
+            document.body.classList.toggle("drawer-sidebar");
+        } else if (windowSize > 650 && document.body.classList.contains("drawer-sidebar")) {
+            document.body.classList.toggle("drawer-sidebar");
+            content.style = "padding-left: 255px;";
+        } else {
+            document.body.classList.toggle("drawer-sidebar");
+            content.style = "padding-left: 0px;";
+        }
     });
 
     var x = document.getElementById("body");
@@ -10,7 +22,12 @@
 
     window.addEventListener("resize", function () {
         if (window.innerWidth < 650) {
+            content.style = "padding-left: 0px;";
             x.classList.add("drawer-sidebar");
+        } else if (window.innerWidth > 650 && document.body.classList.contains("drawer-sidebar")) {
+            content.style = "padding-left: 0px;";
+        } else {
+            content.style = "padding-left: 255px;";
         }
     });
 });
